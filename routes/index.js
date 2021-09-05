2d92b0129168a2edcdb65b2286ecfaad8d60fd2d
Sarath Dev <36992959+Sarath81198@users.noreply.github.com>
2021-09-05 20:47:40 +0530

    Added Employee login API route /emp/login [POST]

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const Employee = require('../model/Employee')
+const EmergencyEvent = require('../model/EmergencyEvent')
 
 router.get("/twillio", async (req, res) => {
     console.log("works")
@@ -13,5 +15,20 @@ router.post("/login", async (req, res) => {
     }
     return res.send({logged_in: true})
 })
+
+router.post("/emp/login", async (req, res) => {
+    const email = req.body.email
+    const phoneNumber = req.body.phone_number
+    const employee = await Employee.findOne({ "email": email })
+
+    if(!employee)
+        return res.send({ employee_logged_in: false, message: "Check you entered email" })
+    
+    if(employee.phone_number !== phoneNumber)
+        return res.send({ employee_logged_in: false, message: "Check you entered password" })
+        
+    return res.send({ employee_logged_in: true, message: "Logged in successfully" })
+
+ })
 
 module.exports = router;
